d5d77bfe53f240d0c6fd69f611ee8f5fc9c5424b
Marwane Jouad <marwanmx3@gmail.com>
2025-04-12 00:56:26 +0100

    Added Spring Boot base structure

---
+++ src/main/java/com/mrm/meetingroommanager/MeetingroommanagerApplication.java
@@ -0,0 +1,13 @@
+package com.mrm.meetingroommanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingroommanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetingroommanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mrm/meetingroommanager/MeetingroommanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrm.meetingroommanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingroommanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
