524efaef21fee5e210dfa28e58769457784bc682
OussamaSaad <f.oussama21@gmail.com>
2025-05-20 10:18:19 +0100

    halo

--- src/main/java/com/mrm/meetingroommanager/entities/Role.java
+++ src/main/java/com/mrm/meetingroommanager/entities/Role.java
@@ -19,6 +19,26 @@ public class Role {
     @OneToMany(mappedBy = "role")
     private List<User> users;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+
     public List<User> getUsers() {
         return users;
     }
